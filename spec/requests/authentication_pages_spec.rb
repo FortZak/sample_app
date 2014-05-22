d35c20864fb5d09a511defc5eccec350288b1316
FortZak <LafortezaK@gmail.com>
2014-05-22 14:04:27 -0400

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -46,6 +46,18 @@ describe "Authentication" do
 
   describe "authorization" do
 
+#    describe "as non-admin user" do
+#      let(:user) { FactoryGirl.create(:user) }
+#      let(:non_admin) { FactoryGirl.create(:user) }
+
+#      before { sign_in non_admin, no_capybara: true }
+
+#      describe "submitting a DELETE request to the Users#destroy action" do
+#        before { delete user_path(user) }
+#        specify { expect(response).to redirect_to(root_url) }
+#      end
+#    end
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -82,19 +94,20 @@ describe "Authentication" do
           it { should have_title('Sign in') }
         end
       end
-    end
 
-#    describe "as non-admin user" do
-#      let(:user) { FactoryGirl.create(:user) }
-#      let(:non_admin) { FactoryGirl.create(:user) }
-#
-#      before { sign_in non_admin, no_capybara: true }
-#
-#      describe "submitting a DELETE request to the Users#destroy action" do
-#        before { delete user_path(user) }
-#        specify { expect(response).to redirect_to(root_url) }
-#      end
-#    end
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
 
 #    describe "as wrong user" do
 #      let(:user) { FactoryGirl.create(:user) }
